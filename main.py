0df7a7b2f4147e346445ecefc1dfd4dbb5a503a1
Manuel Salazar <manalgom@gmail.com>
2022-11-05 10:32:16 -0500

    tweet_post

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ class Tweet(BaseModel):
         max_length=256
     )
     created_at: datetime = Field(default=datetime.now())
-    update_at: Optional[datetime] = Field(default=None)
+    updated_at: Optional[datetime] = Field(default=None)
     by: User = Field(...)
 
 
@@ -247,8 +247,43 @@ def home():
     summary="Post a tweet",
     tags=["Tweets"]
     )
-def post():
-    pass
+def post(tweet: Tweet = Body(...)):
+    """
+    Post a tweet
+
+    Post a new tweet in the app
+
+    Parameters:
+        - Request body parameter
+            - tweet: Tweet
+
+    Returns a json with the basic tweet info:
+        - tweet_id: UUID
+        - content: str
+        - created_at: datetime
+        - updated_at: Optional[datetie]
+        - by: User
+    """
+    ## r+ es lectura y escritura
+    with open("tweets.json", "r+", encoding="utf-8") as f:
+        # results = json.loads(f.read()) # loads (load string), mejor usamos el otro metodo
+        results = json.load(f) # Leemos y guardamos ell file en results
+        tweet_dict = tweet.dict() # Pasamos el Body obj (param de la func) a dict   
+
+        ## Con el param default nos ahorramos toda esta conversión a strings
+        # tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])
+        # tweet_dict["created_at"] = str(tweet_dict["created_at"])
+
+        # if tweet_dict["updated_at"]:
+        #     tweet_dict["updated_at"] = str(tweet_dict["updated_at"])
+
+        # tweet_dict["by"]["user_id"] = str(tweet_dict["by"]["user_id"])
+        # tweet_dict["by"]["birth_date"] = str(tweet_dict["by"]["birth_date"])
+        
+        results.append(tweet_dict)
+        f.seek(0)
+        json.dump(results, f, default=str, indent=4)
+    return tweet
 
 
 ### Show a tweet
